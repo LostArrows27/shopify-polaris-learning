a23bc16a850732c90977101f59dadbc1c88e08ff
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-04-08 11:02:42 +0700

    fix: add validate number in user name string

--- baitap_1/app/schema/address_schema.ts
+++ baitap_1/app/schema/address_schema.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const addressSchema = z.object({
   id: z.string(),
   address: z.string().trim().min(1, "Address is required"),
-  city: z.string().trim().min(1, "City is required"),
+  city: z.string().trim().min(1, "City name is required"),
 });
--- baitap_1/app/schema/user_schema.ts
+++ baitap_1/app/schema/user_schema.ts
@@ -2,7 +2,13 @@ import { z } from "zod";
 import { addressSchema } from "./address_schema";
 
 export const userSchema = z.object({
-  fullName: z.string().trim().min(1, "Full name is required"),
+  fullName: z
+    .string()
+    .trim()
+    .min(1, "Full name is required")
+    .refine((value) => !/\d/.test(value ?? ""), {
+      message: "Full name cannnot contain numbers",
+    }),
   email: z.string().email("Invalid email format"),
   addresses: z.array(addressSchema),
 });
