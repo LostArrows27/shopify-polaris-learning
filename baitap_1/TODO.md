5e50dec35e0c594011631553f505e442996bd491
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-04-08 10:14:35 +0700

    feat: add toast message when save

--- baitap_1/TODO.md
+++ baitap_1/TODO.md
@@ -1,9 +1,4 @@
-- [] add loading state to button when submitting
-- [] add loading state to page when first load 
-  - address
-  - account
-- [] add loading state to avatar when first load
-- [] add toast
+- [x] add toast
 
 - [] add logout function -> confirm modal -> delete all data
 - [] fetch address API + auto complete
--- baitap_1/app/routes/account.tsx
+++ baitap_1/app/routes/account.tsx
@@ -8,6 +8,7 @@ import {
   InlineGrid,
   Text,
   TextField,
+  Toast,
   Tooltip,
 } from "@shopify/polaris";
 import type { Route } from "./+types/home";
@@ -18,7 +19,7 @@ import type { UserFormData } from "~/types/app.type";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { userSchema } from "~/schema/user_schema";
 import { useUserStore } from "~/hooks/use_user_store";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export function meta({}: Route.MetaArgs) {
@@ -31,6 +32,8 @@ export function meta({}: Route.MetaArgs) {
 }
 
 const AccountPage = () => {
+  const [toastActive, setToastActive] = useState(false);
+
   const { fullName, email, addresses, setUserInfo, setAddresses } =
     useUserStore();
 
@@ -71,7 +74,6 @@ const AccountPage = () => {
   }, [fullName, email, addresses, reset]);
 
   const onSubmit = (data: UserFormData) => {
-    console.log("Form data submitted: ", data);
     setUserInfo(data.fullName.trim(), data.email);
     setAddresses(
       data.addresses.map((address) => ({
@@ -81,6 +83,7 @@ const AccountPage = () => {
       }))
     );
     reset(data);
+    setToastActive(true);
   };
 
   const addNewAddressInput = useCallback(() => {
@@ -88,138 +91,149 @@ const AccountPage = () => {
   }, [append]);
 
   return (
-    <div className="w-3/4">
-      <div className="mb-10">
-        <Text variant="headingLg" as="h1" fontWeight="bold">
-          Account
-        </Text>
-      </div>
-      <InlineGrid columns={{ xs: "1fr", md: "3fr 0.2fr 5fr" }} gap="400">
-        <Box as="section">
-          <BlockStack gap="400">
-            <Text as="h3" variant="headingMd">
-              Account details
-            </Text>
-            <Text as="p" variant="bodyMd">
-              Update your account details to keep your store running smoothly.
-            </Text>
-          </BlockStack>
-        </Box>
-        <div></div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <BlockStack gap={"400"}>
-            <Card roundedAbove="sm">
-              <BlockStack gap="400">
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      label="Fullname"
-                      error={errors.fullName?.message}
-                      {...field}
-                      autoComplete="off"
-                    />
-                  )}
-                  name="fullName"
-                  control={control}
-                />
-                <Controller
-                  render={({ field }) => (
-                    <TextField
-                      label="Email"
-                      error={errors.email?.message}
-                      {...field}
-                      autoComplete="off"
-                    />
-                  )}
-                  name="email"
-                  control={control}
-                />
-                <div className="flex w-full justify-end">
-                  <Tooltip dismissOnMouseOut content="Reset">
-                    <Button
-                      icon={UndoIcon}
-                      onClick={() => {
-                        reset({
-                          fullName,
-                          email,
-                        });
-                      }}
-                    />
-                  </Tooltip>
-                </div>
-              </BlockStack>
-            </Card>
-            <Card roundedAbove="sm">
-              <BlockStack gap="400">
-                {fields.length >= 1 ? (
-                  fields.map((field, index) => (
-                    <Box
-                      key={field.id}
-                      borderRadius="300"
-                      padding="400"
-                      background="bg-surface-secondary"
+    <>
+      <div className="w-3/4">
+        <div className="mb-10">
+          <Text variant="headingLg" as="h1" fontWeight="bold">
+            Account
+          </Text>
+        </div>
+        <InlineGrid columns={{ xs: "1fr", md: "3fr 0.2fr 5fr" }} gap="400">
+          <Box as="section">
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Account details
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Update your account details to keep your store running smoothly.
+              </Text>
+            </BlockStack>
+          </Box>
+          <div></div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <BlockStack gap={"400"}>
+              <Card roundedAbove="sm">
+                <BlockStack gap="400">
+                  <Controller
+                    render={({ field }) => (
+                      <TextField
+                        label="Fullname"
+                        error={errors.fullName?.message}
+                        {...field}
+                        autoComplete="off"
+                      />
+                    )}
+                    name="fullName"
+                    control={control}
+                  />
+                  <Controller
+                    render={({ field }) => (
+                      <TextField
+                        label="Email"
+                        error={errors.email?.message}
+                        {...field}
+                        autoComplete="off"
+                      />
+                    )}
+                    name="email"
+                    control={control}
+                  />
+                  <div className="flex w-full justify-end">
+                    <Tooltip dismissOnMouseOut content="Reset">
+                      <Button
+                        icon={UndoIcon}
+                        onClick={() => {
+                          reset({
+                            fullName,
+                            email,
+                          });
+                        }}
+                      />
+                    </Tooltip>
+                  </div>
+                </BlockStack>
+              </Card>
+              <Card roundedAbove="sm">
+                <BlockStack gap="400">
+                  {fields.length >= 1 ? (
+                    fields.map((field, index) => (
+                      <Box
+                        key={field.id}
+                        borderRadius="300"
+                        padding="400"
+                        background="bg-surface-secondary"
+                      >
+                        <BlockStack gap="400">
+                          <Controller
+                            name={`addresses.${index}.address`}
+                            control={control}
+                            render={({ field }) => (
+                              <TextField
+                                label={`Address ${index + 1}`}
+                                autoComplete="off"
+                                error={
+                                  errors.addresses?.[index]?.address?.message
+                                }
+                                {...field}
+                              />
+                            )}
+                          />
+                          <Controller
+                            name={`addresses.${index}.city`}
+                            control={control}
+                            render={({ field }) => (
+                              <TextField
+                                label="City"
+                                autoComplete="off"
+                                error={errors.addresses?.[index]?.city?.message}
+                                {...field}
+                              />
+                            )}
+                          />
+                          <div className="flex w-full justify-end mt-2">
+                            <Tooltip dismissOnMouseOut content="Remove address">
+                              <Button
+                                onClick={() => remove(index)}
+                                icon={DeleteIcon}
+                              />
+                            </Tooltip>
+                          </div>
+                        </BlockStack>
+                      </Box>
+                    ))
+                  ) : (
+                    <EmptyState
+                      heading="Please add your address"
+                      image="https://cdn.shopify.com/s/files/1/2376/3301/products/emptystate-files.png"
                     >
-                      <BlockStack gap="400">
-                        <Controller
-                          name={`addresses.${index}.address`}
-                          control={control}
-                          render={({ field }) => (
-                            <TextField
-                              label={`Address ${index + 1}`}
-                              autoComplete="off"
-                              error={
-                                errors.addresses?.[index]?.address?.message
-                              }
-                              {...field}
-                            />
-                          )}
-                        />
-                        <Controller
-                          name={`addresses.${index}.city`}
-                          control={control}
-                          render={({ field }) => (
-                            <TextField
-                              label="City"
-                              autoComplete="off"
-                              error={errors.addresses?.[index]?.city?.message}
-                              {...field}
-                            />
-                          )}
-                        />
-                        <div className="flex w-full justify-end mt-2">
-                          <Tooltip dismissOnMouseOut content="Remove address">
-                            <Button
-                              onClick={() => remove(index)}
-                              icon={DeleteIcon}
-                            />
-                          </Tooltip>
-                        </div>
-                      </BlockStack>
-                    </Box>
-                  ))
-                ) : (
-                  <EmptyState
-                    heading="Please add your address"
-                    image="https://cdn.shopify.com/s/files/1/2376/3301/products/emptystate-files.png"
-                  >
-                    <p>
-                      Add your address to make sure your orders are shipped to
-                      the right place.
-                    </p>
-                  </EmptyState>
-                )}
-              </BlockStack>
-              <div className="flex mt-5 w-full gap-2 justify-end">
-                <Button onClick={addNewAddressInput}>New Address</Button>
-                <Button submit variant="primary">
-                  Save
-                </Button>
-              </div>
-            </Card>
-          </BlockStack>
-        </form>
-      </InlineGrid>
-    </div>
+                      <p>
+                        Add your address to make sure your orders are shipped to
+                        the right place.
+                      </p>
+                    </EmptyState>
+                  )}
+                </BlockStack>
+                <div className="flex mt-5 w-full gap-2 justify-end">
+                  <Button onClick={addNewAddressInput}>New Address</Button>
+                  <Button submit variant="primary">
+                    Save
+                  </Button>
+                </div>
+              </Card>
+            </BlockStack>
+          </form>
+        </InlineGrid>
+      </div>
+      {toastActive && (
+        <Toast
+          onDismiss={() => {
+            setToastActive(false);
+          }}
+          content="Update successfully!"
+          duration={1000}
+        />
+      )}
+    </>
   );
 };
 
